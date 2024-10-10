205a3c9d2422fcd0dd2c1f73b63a926a309b0abf
hebedebe <babyyodaismine@gmail.com>
2024-10-11 09:16:58 +1100

    added party type and slot verification function

--- scripts/body_part.gd
+++ scripts/body_part.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @export var part_type = "" #leg, arm, eye, etc
+@export var is_minor = false # minor parts can be added into a slot with another part in it 
+#but require a base part
 
 func apply_attributes(player_controller: CharacterBody3D):
 	get_child(0).apply_attributes(player_controller)
--- scripts/body_part_slot.gd
+++ scripts/body_part_slot.gd
@@ -1,8 +1,19 @@
 extends Node
 
+@export var part_type = ""
 
 func manage_body_part(player_controller : CharacterBody3D):
 	var body_part = get_child(0)
 	if body_part == null:
 		return
 	body_part.apply_attributes(player_controller)
+
+func verify_part_validity(part):
+	var body_part = get_child(0)
+	if body_part == null:
+		if part.is_minor:
+			return false
+		else:
+			return (part.part_type == part_type)
+	else:
+		return (part.is_minor and part.part_type == part_type)
