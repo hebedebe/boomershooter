3b51b65fdbea3fa5e5dc3a6fdc6f41c0168990c5
hebedebe <hebedebe1@gmail.com>
2024-10-10 21:05:09 +1100

    fixed materials and added different wall textures

--- scripts/light_flicker.gd
+++ scripts/light_flicker.gd
@@ -4,8 +4,10 @@ extends OmniLight3D
 @export var frequency: float
 var time_passed := 0.0
 var start_energy = 0
+var start_offset = 0
 
 func _ready():
+	start_offset = (position.x + position.z) / 10
 	start_energy = light_energy
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,7 +15,7 @@ func _process(delta):
 	time_passed += delta
 	#print(time_passed)
 	
-	var sampled_noise = noise.noise.get_noise_1d(time_passed * frequency)
+	var sampled_noise = noise.noise.get_noise_1d(time_passed * frequency + start_offset)
 	sampled_noise = abs(sampled_noise)
 	
 	light_energy = start_energy-sampled_noise*(0.7 * start_energy)
